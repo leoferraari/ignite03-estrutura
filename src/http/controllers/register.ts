bcb42847661109bb89751113c6441dc9ee82af75
leoferraari <leonardo.5567@hotmail.com>
2023-05-23 20:32:40 -0300

    Estrutura de Erro - E-mail já existente
    Organização dos erros, criação de classes para definir cada erro no caso de uso.

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 import PrismaUsersRepository from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUserCase } from '@/use-cases/register'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
@@ -27,7 +28,11 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
             password
         })
     } catch(err) {
-        return reply.status(409).send()
+        if (err instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: err.message })
+        }
+
+        return reply.status(500).send()
     }
 
     return reply.status(201).send()
