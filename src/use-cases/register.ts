4f0e912dcebac4ce8acfab04222f70b00ea87f14
leoferraari <leonardo.5567@hotmail.com>
2023-05-23 21:46:42 -0300

    Testes unitários de cadastro

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,6 +2,7 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
     name: string
@@ -9,11 +10,15 @@ interface RegisterUseCaseRequest {
     password: string
 }
 
+interface RegisterUseCaseResponse {
+    user: User
+}
+
 export class RegisterUserCase {
 
     constructor(private usersRepository: UsersRepository) {}
     
-    async execute({    name,    email,     password, }: RegisterUseCaseRequest) {
+    async execute({    name,    email,     password, }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
      
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
     
@@ -23,10 +28,14 @@ export class RegisterUserCase {
     
         const password_hash = await hash(password, 6)
     
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash,
         })
+
+        return { 
+            user,
+        }
     }
 }
