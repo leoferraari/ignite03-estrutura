e6ed982568bc900873e30da356fddb6f6a3dc56f
leoferraari <leonardo.5567@hotmail.com>
2023-05-27 18:20:19 -0300

    TDD e Mocking
    https://app.rocketseat.com.br/node/api-node-js-com-solid/group/implementando-casos-de-uso/lesson/tdd-and-mocking

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -2,7 +2,8 @@ import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { hash } from 'bcryptjs'
-import { expect, describe, it, beforeEach } from 'vitest'
+import { Cipher } from 'crypto'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
 import { CheckInUseCase } from './check-in'
 
@@ -13,6 +14,12 @@ describe('Check-in Use Case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
         checkInUseCase = new CheckInUseCase(checkInsRepository)
+
+        vi.useFakeTimers() //cria o mock
+    })
+
+    afterEach(() => { 
+        vi.useRealTimers() // Antes de cada teste cria o mock deixando as datas ficticias - Usa a data original
     })
 
     it ('should be able to check in', async () => {
@@ -23,5 +30,41 @@ describe('Check-in Use Case', () => {
         })
 
         await expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it ('should not be able to check in twice in the same day', async () => {
+
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+        await checkInUseCase.execute({
+            gymId: 'gym-1',
+            userId: 'user-01',
+        })
+
+        await expect(() => 
+        checkInUseCase.execute({
+            gymId: 'gym-1',
+            userId: 'user-01',
+        }),
+        ).rejects.toBeInstanceOf(Error)
+    })
+
+    it ('should be able to check in twice but in different days', async () => {
+
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+        await checkInUseCase.execute({
+            gymId: 'gym-1',
+            userId: 'user-01',
+        })
+
+        vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+        const { checkIn } = await checkInUseCase.execute({
+            gymId: 'gym-1',
+            userId: 'user-01',
+        })
+
+        expect(checkIn.id).toEqual(expect.any(String))
     })
 })
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -15,13 +15,22 @@ export class CheckInUseCase {
     constructor(private checkInsRepository: CheckInsRepository) {}
 
     async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+            userId,
+            new Date(),
+        )
+
+        if(checkInOnSameDay) {
+            throw new Error()
+        }
+
         const checkIn = await this.checkInsRepository.create({
             gym_id: gymId,
             user_id: userId
         })
+
         return {
             checkIn,
         }
     }
-
 }
