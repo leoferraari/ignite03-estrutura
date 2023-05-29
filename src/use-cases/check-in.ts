8af99169f35d52f519d12d10f2cb80ace723d166
leoferraari <leonardo.5567@hotmail.com>
2023-05-29 19:13:08 -0300

    Validando distância do Check-in
    https://app.rocketseat.com.br/node/api-node-js-com-solid/group/implementando-casos-de-uso/lesson/validando-distancia-do-check-in

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,5 +1,6 @@
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { GymsRepository } from "@/repositories/gyms-repository";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 import { CheckIn } from "@prisma/client";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
@@ -22,15 +23,26 @@ export class CheckInUseCase {
 
     ) {}
 
-    async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    async execute({ userId, gymId, userLatitude, userLongitude }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
         const gym = await this.gymsRepository.findById(gymId)
 
         if (!gym) {
             throw new ResourceNotFoundError()
         }
 
-        //calculate distance between user and gym
+        const distance = getDistanceBetweenCoordinates(
+            { latitude: userLatitude, longitude: userLongitude },
+            {
+                latitude: gym.latitude.toNumber(),
+                longitude: gym.longitude.toNumber(),
+            },
+        )
 
+        const MAX_DISTANCE_IN_KILOMETERS = 0.1
+
+        if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+            throw new Error()
+        }
 
         const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
             userId,
