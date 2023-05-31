9b54b2af45b7ed20ab3b772b6157bb4f24ff0926
leoferraari <leonardo.5567@hotmail.com>
2023-05-31 20:39:41 -0300

    Controller e Rotas - Gyms
    https://app.rocketseat.com.br/node/api-node-js-com-solid/group/controllers-and-testes-e-2-e/lesson/outros-controllers-da-academia

--- src/http/controllers/gyms/create.ts
+++ src/http/controllers/gyms/create.ts
@@ -1,8 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
-
 
 export async function create (request: FastifyRequest, reply: FastifyReply) {
     const createGymBodySchema = z.object({
