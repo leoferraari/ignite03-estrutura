f27e8c7ca46344ce9ca969eedbe2378feb2d0085
leoferraari <leonardo.5567@hotmail.com>
2023-05-30 21:14:48 -0300

    Controller de Perfil e Implementação do Middlaware
    https://app.rocketseat.com.br/node/api-node-js-com-solid/group/controllers-and-testes-e-2-e/lesson/controller-de-perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,17 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-se-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 
 export async function profile (request: FastifyRequest, reply: FastifyReply) {
-    await request.jwtVerify()
+    const getUserProfile = makeGetUserProfileUseCase()
 
-    return reply.status(200).send()
+    const { user } = await getUserProfile.execute({
+        userId: request.user.sub,
+    })
+
+    Reflect.deleteProperty(user, 'password_hash')
+
+    return reply.status(200).send({
+        user
+    })
 }
