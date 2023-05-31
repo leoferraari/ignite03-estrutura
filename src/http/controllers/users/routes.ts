05b79199ed4c385e75b4b2a60f8325640dde27e5
leoferraari <leonardo.5567@hotmail.com>
2023-05-31 20:26:36 -0300

    Controller Criação de Academia e Mudança de Routes
    https://app.rocketseat.com.br/node/api-node-js-com-solid/group/controllers-and-testes-e-2-e/lesson/outros-controllers-da-academia

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,16 @@
+
+import { FastifyInstance } from "fastify";
+import { authenticate } from "./authenticate";
+import { profile } from "./profile";
+import { register } from "./register";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
+
+export async function userRoutes(app: FastifyInstance) {
+    app.post('/users', register)
+
+    app.post('/sessions', authenticate)
+
+
+    /** Authenticated */
+    app.get('/me', { onRequest: [verifyJWT] },  profile)
+}
